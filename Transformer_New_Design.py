647eec6cb1e09be36258e2b56257e843ec8ad7a0
ABDULATEEF AYOOLA MOBOLAJI <110661279+Iamoptimistic@users.noreply.github.com>
2025-08-06 01:57:52 +0100

    Update Transformer_New_Design.py

--- Transformer_New_Design.py
+++ Transformer_New_Design.py
@@ -133,5 +133,5 @@ if user_input:
         response = "I'm not confident about that answer. Try asking a more specific question about a disease or topic."
 
     st.session_state.chat_history.append(("bot", response))
-    st.experimental_rerun()
+
 
